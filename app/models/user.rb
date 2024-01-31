c42162b2562da4784da8d001deaa050d50f9f87e
Takumi Kuji <kranaya094@gmail.com>
2024-02-01 02:33:08 +0900

    Myページ作成とアバター画像を追加できるようにした

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,10 +4,29 @@ class User < ApplicationRecord
   has_many :plants
   has_many :likes
 
+  has_one_attached :avatar
+
   validates :password, length: { minimum: 7 }, if: -> { new_record? || changes[:crypted_password] }
   validates :password, confirmation: true, if: -> { new_record? || changes[:crypted_password] }
   validates :password_confirmation, presence: true, if: -> { new_record? || changes[:crypted_password] }
 
   validates :email, uniqueness: true, presence: true
   validates :name, presence: true, length: { maximum: 255 }
+
+
+  def image_content_type
+    if image.attached? && !image.content_type.in?(%w[image/jpeg image/png image/gif])
+      errors.add(:image, '：ファイル形式が、JPEG, PNG, GIF以外になってます。ファイル形式をご確認ください。')
+    end
+  end
+
+  def image_size
+    if image.attached? && image.blob.byte_size > 1.megabytes
+      errors.add(:image, '：1MB以下のファイルをアップロードしてください。')
+    end
+  end
+
+  def formatted_created_at
+    created_at.strftime('%Y年%m月%d日')
+  end
 end
