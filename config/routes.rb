1051b4a5f59cd69b4acd6a322981cea34e3593a1
Takumi Kuji <kranaya094@gmail.com>
2024-03-22 22:34:00 +0900

    一旦区切り

--- config/routes.rb
+++ config/routes.rb
@@ -13,7 +13,8 @@ Rails.application.routes.draw do
   get 'login', to: 'user_sessions#new'
   post 'login', to: 'user_sessions#create'
   delete 'logout', to: 'user_sessions#destroy'
-
+  get 'product/search', to: 'products#search'
+  
   resources :users do
     get 'plants', to: 'users#search_index'
     post 'callback', to: 'users#callback'
@@ -33,4 +34,6 @@ Rails.application.routes.draw do
       patch 'watered', to: 'plants#watered'
     end
   end
+
+  resources :products
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_02_20_125122) do
+ActiveRecord::Schema[7.0].define(version: 2024_03_21_065320) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -83,6 +83,17 @@ ActiveRecord::Schema[7.0].define(version: 2024_02_20_125122) do
     t.index ["user_id"], name: "index_plants_actions_on_user_id"
   end
 
+  create_table "products", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.string "name"
+    t.integer "price"
+    t.string "rakuten_url"
+    t.string "product_image"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_products_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", null: false
     t.string "crypted_password"
@@ -104,4 +115,5 @@ ActiveRecord::Schema[7.0].define(version: 2024_02_20_125122) do
   add_foreign_key "plants", "users"
   add_foreign_key "plants_actions", "plants"
   add_foreign_key "plants_actions", "users"
+  add_foreign_key "products", "users"
 end
